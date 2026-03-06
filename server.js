2a523ca9ea5971ced98cc6159238b3857255ed58
Shiva0084 <deepmehta@Deeps-MacBook-Air.local>
2026-03-06 12:20:59 +0530

    Update server port configuration for deployment

--- server.js
+++ server.js
@@ -11,9 +11,15 @@ app.use(bodyParser.json());
 
 app.use("/", schoolRoutes);
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+// app.listen(3000, () => {
+//   console.log("Server running on port 3000");
+// });
+// As we need to deploy so we need to make the port such that it is accessible and commertial ready
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+}); 
 app.get("/", (req, res) => {
   res.send("School API is running");
 });
